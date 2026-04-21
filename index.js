3327593e10c50aa0931b4a6c387ac93aee8ab3c1
13666kate <teftelka.666.13@gmail.com>
2026-04-22 03:54:41 +0600

    second commit

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// Находим все наши экраны
-const sections = gsap.utils.toArray(".screen");
-
-sections.forEach((section) => {
-  gsap.to(section, {
-    opacity: 1,
-    scale: 1,
+gsap.utils.toArray(".screen").forEach((section) => {
+  gsap.from(section, {
+    opacity: 0,
+    y: 100,
     duration: 1,
-    ease: "power2.out",
     scrollTrigger: {
       trigger: section,
-      start: "top 50%", // Анимация сработает, когда центр экрана пересечет середину окна
-      toggleActions: "play none none reverse", // При скролле назад анимация повторится
-      onEnter: () => section.classList.add("is-visible"),
+      start: "top 80%"
     }
   });
 });
